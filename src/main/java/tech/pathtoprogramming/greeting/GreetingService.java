b38fff3761fecd4c1d1cd8872f8b14aa0404d0e7
Steven Diamante <stevendiamante13@gmail.com>
2022-01-28 23:04:07 -0500

    Acceptance test passes. Add GreetingServiceTest.

--- src/main/java/tech/pathtoprogramming/greeting/GreetingService.java
+++ src/main/java/tech/pathtoprogramming/greeting/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
     public String getSimpleGreeting() {
-        return null;
+        return "Hello my friend!";
     }
 }
---
+++ src/test/java/tech/pathtoprogramming/greeting/GreetingServiceTest.java
@@ -0,0 +1,16 @@
+package tech.pathtoprogramming.greeting;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GreetingServiceTest {
+
+    private final GreetingService greetingService = new GreetingService();
+
+    @Test
+    void getSimpleGreetingReturnsHelloMyFriend() {
+        String actualGreeting = greetingService.getSimpleGreeting();
+
+        Assertions.assertThat(actualGreeting).isEqualTo("Hello my friend!");
+    }
+}
